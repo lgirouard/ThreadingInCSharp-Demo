e19eb0c5c4c80ccf43e1b657f8a78a42f082cbc3
Logan Girouard <LGIROUARD02@mynbcc.ca>
2023-04-26 12:15:18 -0300

    Added label demo for threading and no thread :)

--- ThreadingUpdateUI/Form1.cs
+++ ThreadingUpdateUI/Form1.cs
@@ -18,21 +18,67 @@ namespace ThreadingUpdateUI
             InitializeComponent();
         }
 
-      
+        private List<Label> threadLabels = new List<Label>();
         private void btnGo_Click(object sender, EventArgs e)
         {
+            if (threadLabels.Count <= 100)
+            {
+                int.TryParse(textBox1.Text, out int iteration);
+                int.TryParse(textBox2.Text, out int sleepDuration);
 
+                Label l = new Label();
+                l.Text = DateTime.Now.ToLongDateString();
+                threadLabels.Add(l);
+
+                l.Top = (panel1.Controls.Count) * l.Height + 20;
+                l.Left = 5;
+
+                this.panel1.Controls.Add(l);
+
+                Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i <= iteration; i++)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            l.Text = i.ToString();
+                        });
+                        Thread.Sleep(sleepDuration);
+                    }
+                });
+
+            }
         }
 
         private void btnGoNoThread_Click(object sender, EventArgs e)
         {
+            int.TryParse(textBox1.Text, out int iteration);
+            int.TryParse(textBox2.Text, out int sleepDuration);
 
+            btnGoNoThread.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+
+            for (int i = 0; i <= iteration; i++)
+            {
+                lblCounter.Text = i.ToString();
+                lblCounter.Refresh();
+                Thread.Sleep(sleepDuration);
+
+                btnGoNoThread.Enabled = true;
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+            }
         }
+
+
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             //We need to kill any running threads
-            
+
+
+
         }
     }
 }
